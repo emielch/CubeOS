11b85dcf43fa71d960170fe6b0b0bcbf32982a6b
Emiel Harmsen <emielch@gmail.com>
2024-05-24 13:57:32 +0200

    update OctoWS2811_Dither

--- Driver/CubeDriver_OK16.h
+++ Driver/CubeDriver_OK16.h
@@ -40,7 +40,6 @@ class CubeDriver_OK16 : public CubeDriver {
   int displayMemory[LEDS_PER_CHANNEL * numPins * 3 / 4];  // * 3 for three bytes per LED, / 4 for 4 bytes in an int
   int drawingMemory[LEDS_PER_CHANNEL * numPins * 3 / 4];
 #if DITHER
-  int writingMemory[LEDS_PER_CHANNEL * numPins * 3 / 4];
   OctoWS2811_Dither *leds;
 #else
   OctoWS2811 *leds;
